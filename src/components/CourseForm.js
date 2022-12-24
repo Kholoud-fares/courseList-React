a5bc28b3a0948d576b2db4f4ac369cf780f44ed4
Kholoud-fares <kholoudshaker79@gmail.com>
2022-12-24 10:40:10 +0200

    course list project

---
+++ src/components/CourseForm.js
@@ -0,0 +1,10 @@
+const CourseForm = (props) =>{
+    return(
+        <form onSubmit={props.addCourse}>
+            <input type="text" value={props.current} onChange={props.updateCourse}/>
+            <button type="submit">Add course</button>
+        </form>
+    )
+}
+
+export default CourseForm;
